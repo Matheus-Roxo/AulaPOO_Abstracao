cd4c6b446a1c329798a75285564d3c5ba1f4b46b
Matheus Roxo Brandão <matheus.roxo.brandao31@gmail.com>
2020-12-23 00:53:10 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AulaPOO_Abstracao.classes;
+
+namespace AulaPOO_Abstracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boleto boleto = new Boleto();
+            
+            boleto.Desconto(10);
+
+            Cartao cartao = new Cartao();
+
+            cartao.Desconto(0);
+
+            Pagamento pagamento = new Pagamento();
+            
+        }
+    }
+}
